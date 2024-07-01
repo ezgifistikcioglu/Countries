7170c9a7066354d91617d7872335f4850d4030ed
ezgieren <fistikciogluezgi@gmail.com>
2024-07-01 18:24:48 +0300

    Integrate Coroutines with ViewModel for database operations
    - Created BaseViewModel class extending AndroidViewModel and CoroutineScope
    - Implemented job management in BaseViewModel to handle coroutine lifecycle
    - Updated FeedViewModel to extend BaseViewModel for coroutine support
    - Added coroutine-based method storeInSQLite to handle database storage operations
    - Integrated coroutine context with Dispatchers.Main for main thread operations
    - Ensured proper job cancellation in onCleared() method of BaseViewModel
    - Modified FeedViewModel to use coroutine launch for database operations
    - Added application context parameter to FeedViewModel constructor for better context management
    
    These changes enable the use of coroutines in ViewModel for efficient and lifecycle-aware database operations.

--- app/src/main/java/com/ezgieren/countries/viewmodel/FeedViewModel.kt
+++ app/src/main/java/com/ezgieren/countries/viewmodel/FeedViewModel.kt
@@ -1,15 +1,17 @@
 package com.ezgieren.countries.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.ezgieren.countries.model.Country
 import com.ezgieren.countries.service.CountryAPIService
+import com.ezgieren.countries.service.CountryDatabase
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.observers.DisposableSingleObserver
 import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
-class FeedViewModel : ViewModel() {
+class FeedViewModel(application: Application) : BaseViewModel(application) {
     private val countryAPIService = CountryAPIService()
     private val disposable = CompositeDisposable()
 
@@ -30,9 +32,7 @@ class FeedViewModel : ViewModel() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(object :DisposableSingleObserver<List<Country>>(){
                     override fun onSuccess(t: List<Country>) {
-                        countries.value = t
-                        countryLoading.value = false
-                        countryError.value = false
+                        storeInSQLite(t)
                     }
 
                     override fun onError(e: Throwable) {
@@ -43,5 +43,26 @@ class FeedViewModel : ViewModel() {
 
                 })
         )
+    }
+    private fun showCountries(countryList : List<Country>){
+        countries.value = countryList
+        countryLoading.value = false
+        countryError.value = false
+    }
+
+    private fun storeInSQLite(list: List<Country>){
+        launch {
+            val dao = CountryDatabase(getApplication()).countryDao()
+            dao.deleteAllCountries()
+            // toTypedArray() -> Bir listeyi tekil elemanlar haline getirmeye olanak tanır
+            // (list to individual) ama list'in başına * koymamız şart yoksa hata verir
+            val listLong = dao.insertAll(*list.toTypedArray())
+            var i = 0
+            while (i < list.size){
+                list[i].uuid = listLong[i].toInt()
+                i++
+            }
+            showCountries(list)
+        }
     }
 }
